cede4c847023a2ee5f473758e28e3a74e7fd2624
Agustín Martínez Fayó <amartinezfayo@gmail.com>
2020-04-30 15:20:49 +0000

    Add spiffetls.Listen* tests and PeerID testing
    Signed-off-by: Agustín Martínez Fayó <amartinezfayo@gmail.com>

--- v2/spiffetls/spiffetls_test.go
+++ v2/spiffetls/spiffetls_test.go
@@ -6,6 +6,7 @@ import (
 	"context"
 	"crypto/tls"
 	"fmt"
+	"io"
 	"net"
 	"os"
 	"sync"
@@ -23,6 +24,11 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	defaultListenProtocol = "tcp"
+	defaultListenLAddr    = "localhost:0"
+)
+
 var (
 	td       = spiffeid.RequireTrustDomainFromString("example.org")
 	clientID = spiffeid.RequireFromString("spiffe://example.org/client-workload")
@@ -30,7 +36,7 @@ var (
 	testMsg  = "Hello!\n"
 )
 
-func TestDialWithMode(t *testing.T) {
+func TestListenAndDial(t *testing.T) {
 	// Common CA for client and server SVIDs
 	ca := test.NewCA(t, td)
 
@@ -82,32 +88,58 @@ func TestDialWithMode(t *testing.T) {
 		listenMode   spiffetls.ListenMode
 		listenOption []spiffetls.ListenOption
 
-		defaultWlAPIAddr   string
-		expErrContains     string
-		usesExternalDialer bool
-		usesBaseTLSConfig  bool
+		defaultWlAPIAddr    string
+		dialErr             string
+		listenErr           string
+		listenLAddr         string
+		listenProtocol      string
+		serverConnPeerIDErr string
+		clientConnPeerIDErr string
+		usesExternalDialer  bool
+		usesBaseTLSConfig   bool
 	}{
 		// Failure Scenarios
 		{
 			name:             "Wrong workload API server socket",
 			dialMode:         spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
 			defaultWlAPIAddr: "wrong-socket-path",
-			expErrContains:   "cannot create X.509 source",
+			dialErr:          "spiffetls: cannot create X.509 source: workload endpoint socket URI must have a tcp:// or unix:// scheme",
+			listenErr:        "spiffetls: cannot create X.509 source: workload endpoint socket URI must have a tcp:// or unix:// scheme",
 		},
 		{
 			name:             "No server listening",
 			dialMode:         spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
 			defaultWlAPIAddr: wlAPIServerB.Addr(),
-			expErrContains:   "unable to dial",
+			dialErr:          "spiffetls: unable to dial: dial tcp: missing address",
+		},
+		{
+			name:           "Invalid server protocol",
+			listenProtocol: "invalid",
+			listenMode:     spiffetls.TLSServerWithSource(wlAPISourceA),
+			listenErr:      "listen invalid: unknown network invalid",
+		},
+		{
+			name:           "Missing server port",
+			listenProtocol: "tcp",
+			listenLAddr:    "invalid",
+			listenMode:     spiffetls.TLSServerWithSource(wlAPISourceA),
+			listenErr:      "listen tcp: address invalid: missing port in address",
+		},
+		{
+			name:           "Invalid server source",
+			listenProtocol: "tcp",
+			listenErr:      "spiffetls: cannot create X.509 source: workload endpoint socket address is not configured",
+			listenMode:     spiffetls.TLSServer(),
 		},
 
-		// Dial Option Scenarios
+		// Dial Option / Listen Option Scenarios
 		{
-			name:              "TLSClient dials using TLS base config",
-			dialMode:          spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
-			listenMode:        spiffetls.TLSServerWithSource(wlAPISourceA),
-			defaultWlAPIAddr:  wlAPIServerB.Addr(),
-			usesBaseTLSConfig: true,
+			name:                "TLSClient dials using TLS base config",
+			dialMode:            spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
+			listenMode:          spiffetls.TLSServerWithSource(wlAPISourceA),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
+			defaultWlAPIAddr:    wlAPIServerB.Addr(),
+			usesBaseTLSConfig:   true,
 			dialOption: []spiffetls.DialOption{
 				spiffetls.WithDialTLSConfigBase(&tls.Config{
 					KeyLogWriter: externalTLSConfBuffer,
@@ -115,11 +147,12 @@ func TestDialWithMode(t *testing.T) {
 			},
 		},
 		{
-			name:               "TLSClient dials using external dialer",
-			dialMode:           spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
-			listenMode:         spiffetls.TLSServerWithSource(wlAPISourceA),
-			defaultWlAPIAddr:   wlAPIServerB.Addr(),
-			usesExternalDialer: true,
+			name:                "TLSClient dials using external dialer",
+			dialMode:            spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
+			listenMode:          spiffetls.TLSServerWithSource(wlAPISourceA),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
+			defaultWlAPIAddr:    wlAPIServerB.Addr(),
+			usesExternalDialer:  true,
 			dialOption: []spiffetls.DialOption{
 				spiffetls.WithDialer(&net.Dialer{
 					Control: func(network, addr string, c syscall.RawConn) error {
@@ -129,13 +162,32 @@ func TestDialWithMode(t *testing.T) {
 				}),
 			},
 		},
+		{
+			name:       "TLSServer with ListenOption",
+			dialMode:   spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
+			listenMode: spiffetls.TLSServerWithSource(wlAPISourceA),
+			listenOption: []spiffetls.ListenOption{
+				spiffetls.WithListenTLSConfigBase(&tls.Config{
+					KeyLogWriter: externalTLSConfBuffer,
+				}),
+			},
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
+			defaultWlAPIAddr:    wlAPIServerB.Addr(),
+		},
 
 		// Defaults Scenarios
 		{
-			name:             "TLSClient succeeds",
-			dialMode:         spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
-			listenMode:       spiffetls.TLSServerWithSource(wlAPISourceA),
-			defaultWlAPIAddr: wlAPIServerB.Addr(),
+			name:                "TLSClient succeeds",
+			dialMode:            spiffetls.TLSClient(tlsconfig.AuthorizeID(serverID)),
+			listenMode:          spiffetls.TLSServerWithSource(wlAPISourceA),
+			defaultWlAPIAddr:    wlAPIServerB.Addr(),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
+		},
+		{
+			name:                "TLSClient succeeds with Dial",
+			listenMode:          spiffetls.TLSServerWithSource(wlAPISourceA),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
+			defaultWlAPIAddr:    wlAPIServerB.Addr(),
 		},
 		{
 			name:             "MTLSClient succeeds",
@@ -144,63 +196,72 @@ func TestDialWithMode(t *testing.T) {
 			defaultWlAPIAddr: wlAPIServerB.Addr(),
 		},
 		{
-			name:             "MTLSWebClient succeeds",
-			dialMode:         spiffetls.MTLSWebClient(webCertPool),
-			listenMode:       spiffetls.MTLSWebServerWithSource(tlsconfig.AuthorizeID(clientID), webCert, wlAPISourceA),
-			defaultWlAPIAddr: wlAPIServerB.Addr(),
+			name:                "MTLSWebClient / MTLSWebServer succeeds",
+			dialMode:            spiffetls.MTLSWebClient(webCertPool),
+			listenMode:          spiffetls.MTLSWebServer(tlsconfig.AuthorizeID(clientID), webCert),
+			defaultWlAPIAddr:    wlAPIServerB.Addr(),
+			clientConnPeerIDErr: "spiffetls: no URI SANs",
 		},
 
 		// *WithSource Scenario
 		{
-			name:       "TLSClientWithSource succeeds",
-			dialMode:   spiffetls.TLSClientWithSource(tlsconfig.AuthorizeID(serverID), wlAPISourceB),
-			listenMode: spiffetls.TLSServerWithSource(wlAPISourceA),
+			name:                "TLSClientWithSource / TLSServerWithSource succeeds",
+			dialMode:            spiffetls.TLSClientWithSource(tlsconfig.AuthorizeID(serverID), wlAPISourceB),
+			listenMode:          spiffetls.TLSServerWithSource(wlAPISourceA),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
 		},
 		{
-			name:       "MTLSClientWithSource succeeds",
+			name:       "MTLSClientWithSource / MTLSServerWithSource succeeds",
 			dialMode:   spiffetls.MTLSClientWithSource(tlsconfig.AuthorizeID(serverID), wlAPISourceB),
 			listenMode: spiffetls.MTLSServerWithSource(tlsconfig.AuthorizeID(clientID), wlAPISourceA),
 		},
 		{
-			name:       "MTLSWebClient  succeeds",
-			dialMode:   spiffetls.MTLSWebClientWithSource(webCertPool, wlAPISourceB),
-			listenMode: spiffetls.MTLSWebServerWithSource(tlsconfig.AuthorizeID(clientID), webCert, wlAPISourceA),
+			name:                "MTLSWebClientWithSource / MTLSWebServerWithSource succeeds",
+			dialMode:            spiffetls.MTLSWebClientWithSource(webCertPool, wlAPISourceB),
+			listenMode:          spiffetls.MTLSWebServerWithSource(tlsconfig.AuthorizeID(clientID), webCert, wlAPISourceA),
+			clientConnPeerIDErr: "spiffetls: no URI SANs",
 		},
 
 		// *WithSourceOptions Scenario
 		{
-			name:       "TLSClientWithSource succeeds",
-			dialMode:   spiffetls.TLSClientWithSourceOptions(tlsconfig.AuthorizeID(serverID), workloadapi.WithClient(wlAPIClientB)),
-			listenMode: spiffetls.TLSServerWithSource(wlAPISourceA),
+			name:                "TLSClientWithSourceOptions / TLSServerWithSourceOptions succeeds",
+			dialMode:            spiffetls.TLSClientWithSourceOptions(tlsconfig.AuthorizeID(serverID), workloadapi.WithClient(wlAPIClientB)),
+			listenMode:          spiffetls.TLSServerWithSourceOptions(workloadapi.WithClientOptions(workloadapi.WithAddr(wlAPIServerA.Addr()))),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
 		},
 		{
-			name:       "MTLSClientWithSource succeeds",
+			name:       "MTLSClientWithSourceOptions / MTLSServerWithSourceOptions succeeds",
 			dialMode:   spiffetls.MTLSClientWithSourceOptions(tlsconfig.AuthorizeID(serverID), workloadapi.WithClient(wlAPIClientB)),
-			listenMode: spiffetls.MTLSServerWithSource(tlsconfig.AuthorizeID(clientID), wlAPISourceA),
+			listenMode: spiffetls.MTLSServerWithSourceOptions(tlsconfig.AuthorizeID(clientID), workloadapi.WithClientOptions(workloadapi.WithAddr(wlAPIServerA.Addr()))),
 		},
 		{
-			name:       "MTLSWebClient  succeeds",
-			dialMode:   spiffetls.MTLSWebClientWithSourceOptions(webCertPool, workloadapi.WithClient(wlAPIClientB)),
-			listenMode: spiffetls.MTLSWebServerWithSource(tlsconfig.AuthorizeID(clientID), webCert, wlAPISourceA),
+			name:                "MTLSWebClientWithSourceOptions / MTLSWebServerWithSourceOptions succeeds",
+			dialMode:            spiffetls.MTLSWebClientWithSourceOptions(webCertPool, workloadapi.WithClient(wlAPIClientB)),
+			listenMode:          spiffetls.MTLSWebServerWithSourceOptions(tlsconfig.AuthorizeID(clientID), webCert, workloadapi.WithClientOptions(workloadapi.WithAddr(wlAPIServerA.Addr()))),
+			clientConnPeerIDErr: "spiffetls: no URI SANs",
 		},
 
 		// *WithRawConfig Scenario
 		{
-			name:       "TLSClientWithSource succeeds",
-			dialMode:   spiffetls.TLSClientWithRawConfig(tlsconfig.AuthorizeID(serverID), bundleSource),
-			listenMode: spiffetls.TLSServerWithSource(wlAPISourceA),
+			name:                "TLSClientWithRawConfig / TLSServerWithRawConfig succeeds",
+			dialMode:            spiffetls.TLSClientWithRawConfig(tlsconfig.AuthorizeID(serverID), bundleSource),
+			listenMode:          spiffetls.TLSServerWithRawConfig(wlAPISourceA),
+			serverConnPeerIDErr: "spiffetls: no peer certificates",
 		},
 		{
-			name:       "MTLSClientWithSource succeeds",
+			name:       "MTLSClientWithRawConfig / MTLSServerWithRawConfig succeeds",
 			dialMode:   spiffetls.MTLSClientWithRawConfig(tlsconfig.AuthorizeID(serverID), svidSource, bundleSource),
-			listenMode: spiffetls.MTLSServerWithSource(tlsconfig.AuthorizeID(clientID), wlAPISourceA),
+			listenMode: spiffetls.MTLSServerWithRawConfig(tlsconfig.AuthorizeID(clientID), wlAPISourceA, bundleSource),
 		},
 		{
-			name:       "MTLSWebClient  succeeds",
-			dialMode:   spiffetls.MTLSWebClientWithRawConfig(webCertPool, svidSource),
-			listenMode: spiffetls.MTLSWebServerWithSource(tlsconfig.AuthorizeID(clientID), webCert, wlAPISourceA),
+			name:                "MTLSWebClientWithRawConfig / MTLSWebServerWithRawConfig succeeds",
+			dialMode:            spiffetls.MTLSWebClientWithRawConfig(webCertPool, svidSource),
+			listenMode:          spiffetls.MTLSWebServerWithRawConfig(tlsconfig.AuthorizeID(clientID), webCert, bundleSource),
+			clientConnPeerIDErr: "spiffetls: no URI SANs",
 		},
 	}
+
+	testClose(t, spiffetls.TLSServerWithSource(wlAPISourceA), spiffetls.TLSClientWithSource(tlsconfig.AuthorizeID(serverID), wlAPISourceB))
 
 	for _, test := range tests {
 		test := test
@@ -221,8 +282,19 @@ func TestDialWithMode(t *testing.T) {
 			var listenAddr string
 			listenDataCh := make(chan string, 1)
 			listenErrCh := make(chan error, 1)
+			if test.listenLAddr == "" {
+				test.listenLAddr = defaultListenLAddr
+			}
+			if test.listenProtocol == "" {
+				test.listenProtocol = defaultListenProtocol
+			}
 			if test.listenMode != nil {
-				listener, err = spiffetls.ListenWithMode(listenCtx, "tcp", "localhost:0", test.listenMode)
+				listener, err = spiffetls.ListenWithMode(listenCtx, test.listenProtocol, test.listenLAddr, test.listenMode, test.listenOption...)
+				if test.listenErr != "" {
+					require.Error(t, err)
+					require.EqualError(t, err, test.listenErr)
+					return
+				}
 				require.NoError(t, err)
 				require.NotNil(t, listener)
 				defer listener.Close()
@@ -244,7 +316,23 @@ func TestDialWithMode(t *testing.T) {
 						return
 					}
 					listenDataCh <- data
+
+					// Test serverConn.PeerID()
+					spiffeID, err := spiffetls.PeerIDFromConn(conn)
+					if test.serverConnPeerIDErr == "" {
+						require.NoError(t, err)
+						require.Equal(t, clientID, spiffeID)
+					} else {
+						require.EqualError(t, err, test.serverConnPeerIDErr)
+					}
 				}()
+			} else {
+				// Test Listen function
+				listener, err = spiffetls.Listen(listenCtx, test.listenProtocol, test.listenLAddr, tlsconfig.AuthorizeID(clientID))
+				if test.listenErr != "" {
+					require.Error(t, err)
+					require.EqualError(t, err, test.listenErr)
+				}
 			}
 
 			// Start dialing
@@ -258,7 +346,17 @@ func TestDialWithMode(t *testing.T) {
 			wg.Add(1)
 			go func() {
 				defer wg.Done()
-				conn, err := spiffetls.DialWithMode(dialCtx, "tcp", listenAddr, test.dialMode, test.dialOption...)
+				var conn net.Conn
+				var err error
+				if test.dialMode == nil {
+					// Test Dial function
+					conn, err = spiffetls.Dial(dialCtx, "tcp", listenAddr, tlsconfig.AuthorizeID(serverID))
+				} else {
+					conn, err = spiffetls.DialWithMode(dialCtx, "tcp", listenAddr, test.dialMode, test.dialOption...)
+				}
+				if len(test.listenOption) > 0 {
+					require.NotEmpty(t, externalTLSConfBuffer.Len())
+				}
 				if err != nil {
 					dialErrCh <- err
 					return
@@ -281,6 +379,15 @@ func TestDialWithMode(t *testing.T) {
 						require.NotEmpty(t, externalTLSConfBuffer.Len())
 					}
 
+					// Test clientConn.PeerID()
+					spiffeID, err := spiffetls.PeerIDFromConn(dialConn)
+					if test.clientConnPeerIDErr == "" {
+						require.NoError(t, err)
+						require.Equal(t, serverID, spiffeID)
+					} else {
+						require.Error(t, err)
+					}
+
 					fmt.Fprint(dialConn, testMsg)
 
 				case data := <-listenDataCh:
@@ -291,8 +398,8 @@ func TestDialWithMode(t *testing.T) {
 					t.Fatalf("Listener failed: %v\n", err)
 
 				case err := <-dialErrCh:
-					if test.expErrContains != "" {
-						require.Contains(t, err.Error(), test.expErrContains)
+					if test.dialErr != "" {
+						require.EqualError(t, err, test.dialErr)
 						return
 					}
 					require.NoError(t, err)
@@ -306,6 +413,55 @@ func TestDialWithMode(t *testing.T) {
 			}
 		})
 	}
+}
+
+// testClose tests closing connections and listener
+func testClose(t *testing.T, listenMode spiffetls.ListenMode, dialMode spiffetls.DialMode) {
+	listenCtx, cancelListenCtx := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelListenCtx()
+	listener, err := spiffetls.ListenWithMode(listenCtx, defaultListenProtocol, defaultListenLAddr, listenMode)
+	require.NoError(t, err)
+
+	go func() {
+		conn, err := listener.Accept()
+		require.NoError(t, err)
+
+		_, err = io.Copy(conn, conn)
+		require.NoError(t, err)
+
+		conn.Close()
+	}()
+
+	dialCtx, cancelDialCtx := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancelDialCtx()
+	conn, err := spiffetls.DialWithMode(dialCtx, "tcp", listener.Addr().String(), dialMode)
+	require.NoError(t, err)
+
+	// Test writing data to the connection
+	dataString := "test data"
+	n, err := conn.Write([]byte(dataString))
+	require.NoError(t, err)
+	require.Equal(t, len(dataString), n)
+
+	// Close connection
+	require.NoError(t, conn.Close())
+
+	// If the connection was really closed, this should fail
+	_, err = conn.Write([]byte(dataString))
+	require.EqualError(t, err, "tls: use of closed connection")
+
+	// Connection has been closed already, expect error
+	require.EqualError(t, conn.Close(), "spiffetls: unable to close TLS connection: tls: use of closed connection")
+
+	// Close listener
+	require.NoError(t, listener.Close())
+
+	// If the listener was really closed, this should fail
+	_, err = listener.Accept()
+	require.Error(t, err)
+
+	// Listener has been closed already, expect error
+	require.Contains(t, listener.Close().Error(), "use of closed network connection")
 }
 
 func setWorkloadAPIResponse(ca *test.CA, s *fakeworkloadapi.WorkloadAPI, spiffeID spiffeid.ID) {
